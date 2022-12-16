1df822c82678c41e77ac4ebb9ff39ec136e5992f
Sang Uk Jung <83222288+JSU0318@users.noreply.github.com>
2022-12-16 14:27:39 +0900

    test3

--- src/components/recipe/Recipe.jsx
+++ src/components/recipe/Recipe.jsx
@@ -345,15 +345,15 @@ const CommentSize = styled.h2`
   color: white;
 `;
 
-// const StDiv = styled.div`
-//   position: absolute;
-//   /* top: 0;
-//   left: 0; */
-//   /* width: 100%; */
-//   /* height: 100%; */
-//   background: url(https://source.unsplash.com/random/1920x1080);
-//   background-size: cover;
-// `;
+const StDivs = styled.div`
+  position: absolute;
+  /* top: 0;
+  left: 0; */
+  /* width: 100%; */
+  /* height: 100%; */
+  background: url(https://source.unsplash.com/random/1920x1080);
+  background-size: cover;
+`;
 
 const StP = styled.p`
   padding-left: 25px;
